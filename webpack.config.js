c4904e2a7b592e6a7b4cda6258cf6daba465e5a2
Tomasz Paterka <tomekpaterka@gmail.com>
2018-04-20 22:50:29 +0200

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+module.exports = {
+	entry: [
+		'react-hot-loader/patch',
+		'./src/index.js'
+	],
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		}),
+		new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeJsPlugin({
+			sourceMap: false
+		})
+	]
+};
+
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+	new HtmlWebpackPlugin({
+		template: 'src/index.html',
+		filename: 'index.html',
+		inject: 'body',
+	})
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeJsPlugin({
+			sourceMap: false
+		})
+	);
+}
